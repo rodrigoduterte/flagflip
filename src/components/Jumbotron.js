4d23fe184ce9786dc1822743931dcfd5aac5506d
rodrigoduterte <gabohermoso@gmail.com>
2018-09-24 01:35:18 -0400

    final files

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -3,7 +3,7 @@ import React from "react";
 const Jumbotron = props => <div className="jumbotron text-center"><div className="overlay" />
       <div className="background-image"/><div className="caption">
         <h1>Raise the Flag!</h1>
-        <p>Click an image and gain a point. Click the same image twice and you'll lose points</p>
+        <p>Click an image and gain a point. Click the same image twice and you'll loose points</p>
       </div>
     </div>
 
